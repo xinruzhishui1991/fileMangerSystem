5c1d8d2b64d5ad88081c2077318f36d04d6b4185
李晓兵 <root@lixiaobing-VirtualBox.(none)>
2017-12-22 15:06:00 +0800

    lxb 基础框架搭建好了;

---
+++ thinkphp5/application/index/controller/Register.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 17-12-19
+ * Time: 下午1:58
+ */
+
+namespace app\index\controller;
+use think\Controller;
+
+class Register extends Controller
+{
+
+    /*
+     * 注册页面
+     */
+    function registTest(){
+        return 'fuck the dog !';
+        //return $this->view();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
